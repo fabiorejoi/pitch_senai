a765095bc221c23999e36d44d4243a3aad87663f
Fábio Martinez <fabiorejoi@gmail.com>
2025-12-17 12:31:29 -0300

    feat: implementa endpoints GET e POST de usuários

--- server.js
+++ server.js
@@ -5,14 +5,23 @@ app.use(express.json())
 
 const users = []
 
+// POST - cadastrar usuário
 app.post('/usuarios', (req, res) => {
+    const usuario = req.body
 
-    users.push(req.body)
-    res.status(201).json(req.body)
+    users.push(usuario)
+
+    res.status(201).json({
+        mensagem: 'Usuário cadastrado com sucesso',
+        usuario
+    })
 })
 
+// GET - listar usuários
 app.get('/usuarios', (req, res) => {
     res.status(200).json(users)
 })
 
-app.listen(3000)
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000')
+})
